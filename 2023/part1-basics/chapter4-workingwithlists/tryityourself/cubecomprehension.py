8ad075d4fcc29ca21e392ae57d1f932b1652ace8
ctrlshftejct <harveyap20@gmail.com>
2023-09-23 14:14:55 +0900

    try it yourself

---
+++ 2023/part1-basics/chapter4-workingwithlists/tryityourself/cubecomprehension.py
@@ -0,0 +1,5 @@
+# cube and cube comprehension
+# print list of first 10 cubes
+
+cubes = [value**3 for value in range(1, 11)]
+print(cubes)
---
+++ 2023/part1-basics/chapter4-workingwithlists/tryityourself/threes.py
@@ -0,0 +1,5 @@
+# threes
+# make a list of multiples from 3-30 using a for loop
+
+threes = [value*3 for value in range(1, 11)]
+print(threes)
